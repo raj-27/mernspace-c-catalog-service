2e463c42ccf2999983f6a1680f75e87d64c77ca2
Raj <raj819314@gmail.com>
2024-07-18 13:21:18 +0530

    add product model

---
+++ src/product/product-model.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const attributesSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    value: {
+        type: mongoose.Schema.Types.Mixed,
+    },
+});
+
+const priceConfigurationSchema = new mongoose.Schema({
+    priceType: {
+        type: String,
+        enum: ["base", "additional"],
+    },
+    availableOptions: {
+        type: Map,
+        of: Number,
+    },
+});
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        priceConfiguration: {
+            type: Map,
+            of: priceConfigurationSchema,
+        },
+        attributes: [attributesSchema],
+        tenantId: {
+            type: String,
+            reuired: true,
+        },
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+        },
+        isPublish: {
+            type: Boolean,
+            require: true,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model("product", productSchema);
